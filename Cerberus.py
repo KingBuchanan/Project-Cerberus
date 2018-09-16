bf79e98da0191df7010e5324744b9975a9159106
Hakeem Buchanan <hakeemsbuchanan@outlook.com>
2018-09-15 22:49:14 -0400

    First Commit
    Added start of python program.
    
    -Cerberus in puppy stage.

---
+++ Cerberus.py
@@ -0,0 +1,34 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24)
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "copyright", "credits" or "license()" for more information
+
+>>>import speech_recognition
+import pyttsx
+
+speech_engine = pyttsx.init('sapi5') # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init
+speech_engine.setProperty('rate', 150)
+
+def speak(text):
+	speech_engine.say(text)
+	speech_engine.runAndWait()
+
+recognizer = speech_recognition.Recognizer()
+
+def listen():
+	with speech_recognition.Microphone() as source:
+		recognizer.adjust_for_ambient_noise(source)
+		audio = recognizer.listen(source)
+
+	try:
+		return recognizer.recognize_sphinx(audio)
+		# or: return recognizer.recognize_google(audio)
+	except speech_recognition.UnknownValueError:
+		print("Could not understand audio")
+	except speech_recognition.RequestError as e:
+		print("Recog Error; {0}".format(e))
+
+	return ""
+
+speak("Say something!")
+speak("I heard you say " + listen())
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Project-Cerberus
-A personal project of mine, creating a voice assistant that can respond to human input and perform tasks. The base project will perform simple tasks, the projected final with incorporate machine learning algorithms to make Cerberus "smarter"
+A personal project of mine, creating a voice assistant that can respond to human input and perform tasks. The base project will perform simple tasks, the projected final with incorporate machine learning algorithms to make Cerberus "smarter". 
+
+Most code used is based on resources found online. 
+Resources:
+ https://pythonspot.com/personal-assistant-jarvis-in-python/
+ https://ggulati.wordpress.com/2016/02/24/coding-jarvis-in-python-3-in-2016/
